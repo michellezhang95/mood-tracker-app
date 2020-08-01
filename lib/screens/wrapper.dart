30f11c28e8bfc6904bdc894b1bf739f4de6eda93
michellezhang95 <michelle.zhang56@gmail.com>
2020-08-01 15:09:07 +1200

    Updated register with email and password

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:mood_tracker_app/authenticate/authenticate.dart';
+import 'package:mood_tracker_app/models/user.dart';
+import 'package:mood_tracker_app/screens/home.dart';
 
 //this wrapper will determine which screen is displayed
 // sign in or register
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,3 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class DatabaseService {}
+class DatabaseService {
+  final String uid;
+  //constructor sets uid
+  DatabaseService({this.uid});
+
+  final CollectionReference moodCollection =
+      Firestore.instance.collection('moods');
+
+  Future updateUserData(String name) async {
+    return await moodCollection.document(uid).setData({
+      'name': name,
+    });
+  }
+}
